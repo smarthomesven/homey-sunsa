72b767f900d26218d32da5c66dfca48ca1636bd1
smarthomesven <sven@dypodex.com>
2026-03-19 08:02:17 +0100

    Code cleanup

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 
 const Homey = require('homey');
 
-module.exports = class MyApp extends Homey.App {
+module.exports = class SunsaApp extends Homey.App {
 
   /**
    * onInit is called when the app is initialized.
    */
   async onInit() {
-    this.log('MyApp has been initialized');
+    this.log('SunsaApp has been initialized');
   }
 
 };
